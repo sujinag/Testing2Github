d1625a28647e4d7de26aeee7346787aac734fc86
sujeet <nag.sujeet@gmail.com>
2021-03-14 18:33:49 +0530

    name edited

--- Testing2Github/ViewController.swift
+++ Testing2Github/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("name created")
+        print("name createdd")
         self.view.backgroundColor = .systemPink
 
         // Do any additional setup after loading the view.
