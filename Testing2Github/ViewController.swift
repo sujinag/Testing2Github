9fc2dd5624d6bd37074e6352d285740a11618ce5
sujeet <nag.sujeet@gmail.com>
2021-03-14 18:57:02 +0530

    name print

--- Testing2Github/ViewController.swift
+++ Testing2Github/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("sujeet")
         // Do any additional setup after loading the view.
     }
 
